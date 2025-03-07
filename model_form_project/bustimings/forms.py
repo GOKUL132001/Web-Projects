018790b70987c1729f2b597655102534e80c6ab4
GOKUL132001 <gokulgurusamy014@gmail.com>
2025-03-07 19:48:53 +0530

    Add files via upload

---
+++ model_form_project/bustimings/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import BusTiming
+import datetime
+
+class BusTimingForm(forms.ModelForm):
+    class Meta:
+        model = BusTiming
+        fields = '__all__'
